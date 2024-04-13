7bdd07ad0388aeb50656e1ba796b2a876b22b3cc
Arianna DeNardo <156139818+AriannaDeNardo@users.noreply.github.com>
2024-04-12 22:56:47 -0400

    Update gitprofile.config.ts

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -25,7 +25,7 @@ const CONFIG = {
       },
       manual: {
         // Properties for manually specifying projects
-        projects: ['AriannaDeNardo/gitprofile'], // List of repository names to display. example: ['arifszn/my-project1', 'arifszn/my-project2']
+        projects: ['AriannaDeNardo/portfolio'], // List of repository names to display. example: ['arifszn/my-project1', 'arifszn/my-project2']
       },
     },
     external: {
